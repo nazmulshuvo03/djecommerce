61732d61332530d38435ce32515cf7caf2551288
nazmulshuvo03 <nazmulshuvo03@gmail.com>
2020-10-12 17:36:58 +0600

    add item ot cart created

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import checkout, HomeView, ItemDetailView
+from .views import checkout, HomeView, ItemDetailView, add_to_cart
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name="item-list"),
     path('checkout/', checkout, name="checkout"),
-    path('product/<slug>', ItemDetailView.as_view(), name="product")
+    path('product/<slug>', ItemDetailView.as_view(), name="product"),
+    path('add-to-cart/<slug>/', add_to_cart, name="add-to-cart")
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
+from django.shortcuts import redirect
+from django.utils import timezone
 
-from .models import Item
+from .models import Item, OrderItem, Order
 
 
 class HomeView(ListView):
@@ -16,3 +18,27 @@ def checkout(request):
 class ItemDetailView(DetailView):
     model = Item
     template_name = "product.html"
+
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item, user=request.user, ordered=False)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+
+        else:
+            order.items.add(order_item)
+
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+
+    return redirect("core:product", slug=slug)
